b786d04501957706b9044a8a94f2598c40ef91dd
Bruno Ribeiro <brunorbds50@gmail.com>
2021-11-24 21:40:57 -0300

    Adding service

--- src/app/Components/user-selection/user-selection.component.ts
+++ src/app/Components/user-selection/user-selection.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ApiNetflixService } from 'src/app/Services/api-netflix.service';
 import { LoginUserInformation } from 'src/app/Services/endpoints-results';
 
 @Component({
@@ -11,9 +12,10 @@ export class UserSelectionComponent implements OnInit {
 
   usersSelection: Array<LoginUserInformation> = [];
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private api: ApiNetflixService) { }
 
   ngOnInit(): void {
+    this.usersSelection = this.api.listOfUsers
   }
 
   sendUserToMain(user: LoginUserInformation){
