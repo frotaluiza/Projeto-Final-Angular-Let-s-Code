8f23201b9547f4ac21e141cb5a103240ac6736a8
frotaluiza <frota.luiza@gmail.com>
2021-11-24 21:43:34 -0300

    commit da página estática

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './Components/main-page/main-page.component';
 import { ButtonComponent } from './Components/Shared/button/button.component';
+import { LandingPageComponent } from './Components/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     ButtonComponent,
+    LandingPageComponent,
   ],
   imports: [
     BrowserModule,
